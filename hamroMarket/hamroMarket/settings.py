a3b43e8ac1ce7e78345220746805131c366b22bf
Sanjeev Theeng Tamang <sanzeevtamang@gmail.com>
2019-02-27 09:28:35 +0545

    latest

--- hamroMarket/hamroMarket/settings.py
+++ hamroMarket/hamroMarket/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'hamroMarket.wsgi.application'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 # Database
